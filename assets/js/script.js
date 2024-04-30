cfb0e780dec8b65d34a8bbbbd1ed17aca6da1119
srilakshmikonda17 <srilakshmikonda17@gmail.com>
2024-04-30 15:26:24 -0700

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Civil Engineer"],
+    strings: ["Civil Engineer , Project Engineer, Construction Manager"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
